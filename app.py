966e8d60ff75153874342783180665bbff34f506
Tharun R S <rstharun11@gmail.com>
2023-09-06 23:02:46 +0530

    front page, hero and about us sect-1

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    #return "hello world"
+def homepage():
     return render_template('index.html', app_name='BulkBois', description='For the GymBros')
 
 if __name__ == '__main__':
